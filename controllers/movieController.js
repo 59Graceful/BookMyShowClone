1f2c7f7dafa1ee8602722e59607a95204d05c107
59Graceful <73896035+59Graceful@users.noreply.github.com>
2026-01-04 18:07:11 +0530

    ready for deployment

---
+++ controllers/movieController.js
@@ -0,0 +1,85 @@
+const Movie = require('../models/movieModel');
+const AddMovie = async (req, res) => {
+    try{
+        const newMovie = new Movie(req.body);
+        await newMovie.save();
+        res.send({
+            success: true,
+            message: "Movie Added Successfully!"
+        })
+    }catch(error){
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+const getAllMovies = async (req, res)=>{
+    try{
+        const movies = await Movie.find();
+        res.send({
+            success: true,
+            message: "Movies Fetched Successfully",
+            data: movies
+        })
+    }catch(error){
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+const deleteMovies = async(req,res) => {
+    try {
+        await Movie.findByIdAndDelete(req.body.movieId);
+        res.send({
+            success: true,
+            message: "Movie Deleted Successfully"
+        })
+    } catch (err) {
+        res.send({
+            success:false,
+            message: err.message
+        })
+    }
+}
+
+const updateMovie = async(req, res)=>{
+    try{
+        await Movie.findByIdAndUpdate(req.body.movieId, req.body);
+        res.send({
+            success: true,
+            message: "Movie Updated Successfully"
+        })
+    }catch(err){
+        res,send({
+            success: false,
+            message: err.message
+        })
+    }
+}
+const getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        res.send({
+            success: true,
+            message: "Movie Fetched Successfully",
+            data: movie
+        })
+    } catch(error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+}
+module.exports ={
+    AddMovie,
+    getAllMovies,
+    deleteMovies,
+    updateMovie,
+    getMovieById
+}
---
+++ controllers/userController.js
@@ -0,0 +1,70 @@
+const userModel = require("../models/userModel")
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+    try {
+        const userExists = await userModel.findOne({ email: req.body.email });
+        if (userExists) {
+            return res.send({
+                success: false,
+                message: 'User Already Exists'
+            })
+        }
+        // hashing the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword
+        const user = new userModel(req.body);
+        const response = await user.save();
+        console.log(response);
+        res.send({ "success": true, "message": "Registration Successful, Please login" })
+    } catch (e) {
+        console.log(e);
+        res.send({ "success": false, "message": e.message })
+    }
+}
+
+const LoginUser = async(req, res) => {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+        return res.send({
+            success: false,
+            message: 'User does not exists'
+        })
+    }
+
+    const validatePassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validatePassword){
+        return res.send({
+            success: false,
+            message: 'Incorrect Password'
+        })
+    }
+    const token = jwt.sign({userId:user._id}, process.env.SecretKey ,{expiresIn: '1d'})
+    res.send({
+        success: true,
+        message: 'User Logged In',
+        data: token
+    })
+}
+const getCurrentUserInfo = async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user.userId).select('-password')
+        res.send({
+            success : true,
+            message : 'User details fetched Successfully',
+            data : user
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+          });
+    }
+}
+module.exports = {
+    registerUser,
+    LoginUser,
+    getCurrentUserInfo
+}
