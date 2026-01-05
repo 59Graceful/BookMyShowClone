7ed363ce5afb5a559b6b8b00d26621c93f277a58
59Graceful <73896035+59Graceful@users.noreply.github.com>
2026-01-05 23:03:56 +0530

    payment-success fix3

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 // React SPA catch-all
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
